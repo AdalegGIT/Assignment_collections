d25c54f28b4efa2424afaa08e10d8fe88cbf40ce
Harisree <harisree.n.s@gmail.com>
2021-01-03 18:52:04 -0500

    Create class_exercise04.rb

---
+++ class_exercise04.rb
@@ -0,0 +1,7 @@
+
+# Write code to generate a hash where the keys are the numbers from 1 to 50 and the values follow these rules:
+#
+# if the number is divisible by 2 the value should be one more than the key
+# if the number is divisible by 7 the value should be one less than the key
+# if the number is divisible by 2 and 7 the value should be the key multiplied by 2
+# otherwise the value should be the same number as the key
