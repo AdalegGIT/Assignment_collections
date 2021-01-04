6b32f46d08495797390c043bfc00735ae287b70a
Harisree <harisree.n.s@gmail.com>
2021-01-03 19:03:45 -0500

    Create class_exercise06.rb

---
+++ class_exercise06.rb
@@ -0,0 +1,61 @@
+# Write a method to assign Country Code to each user, make use of both the arrays provided.
+
+    user_data = [
+        {
+          id: 1,
+          first_name: "Hubie",
+          last_name: "Haggerwood",
+          country: "Belgium",
+        },
+        {
+          id: 2,
+          first_name: "Dyna",
+          last_name: "Kulis",
+          country: "New Zealand",
+        },
+        {
+          id: 3,
+          first_name: "Malorie",
+          last_name: "Shackle",
+          country: "India",
+        },
+        {
+          id: 4,
+          first_name: "Marjie",
+          last_name: "Willimot",
+          country: "Brazil",
+        },
+        {
+          id: 5,
+          first_name: "Arne",
+          last_name: "Abrahamsohn",
+          country: "Brazil",
+        },
+      ];
+  
+      country_code = [
+        {
+          country: {
+            name: "EG",
+            code: "Belgium",
+          },
+        },
+        {
+          country: {
+            name: "NZE",
+            code: "New Zealand",
+          },
+        },
+        {
+          country: {
+            name: "BR",
+            code: "Brazil",
+          },
+        },
+        {
+          country: {
+            name: "IN",
+            code: "India",
+          },
+        },
+      ];
